0330144dcea34d669df92919d810e3164f9526ed
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-10-23 19:34:39 -0300

    configured users migration

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,17 @@
 import setupKnex from "knex";
 import { env } from "./env";
+import { Knex } from "knex";
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: "sqlite",
   connection: {
     filename: env.DATABASE_URL,
-    port: env.PORT,
+  },
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
   },
   useNullAsDefault: true,
-});
+};
+
+export const knex = setupKnex(config);
